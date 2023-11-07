cf845606c40eb4da512a7a3069548414fa460abd
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-06 22:43:46 -0500

    adds lua core to test customrc

--- flake.nix
+++ flake.nix
@@ -12,19 +12,49 @@
       (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          neovimConfig = { };
+          extraPackages = [ ];
+          plugins = with pkgs.vimPlugins; [
+            telescope-nvim
+            nvim-treesitter.withAllGrammars
+          ];
+          binPath = pkgs.lib.makeBinPath extraPackages;
+          initFile = pkgs.writeTextFile {
+            name = "init.lua";
+            text = ''
+              vim.loader.enable()
+              vim.opt.rtp:append("${../.}")
+              require "core"
+            '';
+          };
+          neovimConfig = pkgs.neovimUtils.makeNeovimConfig
+            {
+              customRC = "luafile ${initFile}";
+            } // {
+            viAlias = true;
+            vimAlias = true;
+            withNodeJs = false;
+            withPython3 = false;
+            withRuby = false;
+            wrapperArgs = pkgs.lib.escapeShellArgs [ "--suffix" "PATH" ":" "${binPath}" ];
+            packpathDirs.myNeovimPackages = {
+              start = plugins;
+              opt = [ ];
+            };
+          };
         in
         with pkgs;
         rec {
-          packages.default = wrapNeovimUnstable neovim-unwrapped neovimConfig;
-          apps.default = flake-utils.lib.mkApp {
+          packages.nix-nvim = wrapNeovimUnstable neovim-unwrapped neovimConfig;
+          packages.default = packages.nix-nvim;
+          apps.nix-nvim = flake-utils.lib.mkApp {
             drv = packages.nix-nvim;
             name = "nix-nvim";
             exePath = "/bin/nvim";
           };
+          apps.default = apps.nix-nvim;
           devShell = mkShell {
             buildInputs = [
-              packages.default
+              packages.nix-nvim
             ];
           };
         });
